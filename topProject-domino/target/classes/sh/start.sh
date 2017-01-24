9a91252ad6b387011d45beb227e0c2437a6f1673
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2017-01-24 20:39:10 +0900

    remove files

--- topProject-domino/target/classes/sh/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export LANG=en_US.UTF-8
-export JAVA_HOME=/opt/daou/TMA2/3rd/java
-
-JAVA=/opt/daou/TMA2/3rd/java/bin/java
-DOMINO_ROOT=/opt/daou/TMA2
-
-$JAVA -jar -server -Xms1024M -Xmx2048M -Dlogback.configurationFile=$DOMINO_ROOT/config/logback.xml -Djournal.configurationFile=$DOMINO_ROOT/config/domino.config $DOMINO_ROOT/bin/domino-crawler.jar &
--- topProject-domino/target/classes/sh/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/bin/ps -ef | grep /opt/daou/TMA2/bin/domino-crawler.jar | grep -v grep | awk '{print $2}' | xargs kill -15
