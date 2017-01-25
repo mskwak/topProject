dd0a09aaa0ad98150b6cb52d8a47442582cfbc3d
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2017-01-25 18:17:08 +0900

    commit!!!

--- topProject-executable-jar/topProject-executable-jar-smtp-sender/src/main/resources/start.sh
+++ topProject-executable-jar/topProject-executable-jar-smtp-sender/src/main/resources/start.sh
@@ -3,7 +3,9 @@
 JAVA_HOME=/opt/java
 SMTP_CLIENT_HOME=/opt/data/smtpClient
 
-pidof smtp-sink | xargs kill -9
-smtp-sink -u root 0.0.0.0:25 1024 &
+if [ -e "/usr/sbin/smtp-sink" ]; then
+	pidof smtp-sink | xargs kill -9
+	smtp-sink -u root 0.0.0.0:25 1024 &
+fi
 
 $JAVA_HOME/bin/java -jar  -Dproperties.file.path=$SMTP_CLIENT_HOME/smtpClient.properties -Dlogback.configurationFile=$SMTP_CLIENT_HOME/logback.xml smtpClient.jar
