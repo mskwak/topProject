942fa022df27cd25a003590736ada6271e06dbaa
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2017-02-13 23:33:16 +0900

    commit!!!

---
+++ topProject-executable-jar/topProject-executable-jar-mime-parser/src/main/java/com/hangugi/MimeParserMain.java
@@ -0,0 +1,32 @@
+package com.hangugi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+public class MimeParserMain {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("D:\\eml");
+			//File file = new File("D:\\2011.���Ϲ��");
+			File[] files = file.listFiles();
+
+			for (File f : files) {
+				if(f.isFile()) {
+					MimeMessage mimeMessage = new MimeMessage(null, new FileInputStream(f.getAbsolutePath()));
+					MimeParser mimeParser = new MimeParser();
+					mimeParser.setMimeMessage(mimeMessage);
+					mimeParser.printStructure();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+}
